1cfa83cb4ca18743fe674afc8b271c2097760353
RodrigoMarquesPF <rodrigomarquespf@gmail.com>
2025-01-08 12:44:15 -0300

    Inicio projeto

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500">
+      <h1 className="text-white text-3xl font-bold">Gestão de Banca de Apostas</h1>
+    </div>
+  );
+}
+
+export default App;
+
